0cf0f2f1d9afc90de8dfead373c1a8a984998790
blaise2s <blaise.schaeffer@gmail.com>
2020-03-23 00:08:26 -0400

    chore(RENAME): Specify angular implementation

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,10 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/neutron-components'),
+      dir: require('path').join(
+        __dirname,
+        './coverage/neutron-components-angular'
+      ),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
